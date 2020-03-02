e1b19513ba7382baedbe314b882ba70cd83d2dda
Iarlen Aquiles <iarlensilva775@gmail.com>
2020-03-01 21:59:56 -0300

    testando eslint

--- app/Models/Team.js
+++ app/Models/Team.js
@@ -1,14 +1,14 @@
 "use strict";
 
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
-const Model = use("Model");
+const Model = use("Model")
 
 class Team extends Model {
-  users() {
+  users () {
     return this.belongsToMany("App/Models/User").pivotModel(
       "App/Models/UserTeam"
     );
   }
 }
 
-module.exports = Team;
+module.exports = Team
